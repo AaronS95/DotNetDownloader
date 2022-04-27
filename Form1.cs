6e9254801df936b2b2f3a416be8ca93de4cabb27
Aaron Slater <aslater@htec.co.uk>
2022-04-27 10:02:56 +0100

    Put MessageBox back once installer has downloaded

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.Diagnostics;
@@ -75,15 +68,15 @@ namespace DotNetDownloader
         private static void Download(string url, string installer)
         {
             using (var wc = new WebClient())
-            try
-            {
-                wc.DownloadFile(url, installer);
-                //MessageBox.Show("Installer downloaded!");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+                try
+                {
+                    wc.DownloadFile(url, installer);
+                    MessageBox.Show("Installer downloaded!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
         }
 
         private void button1_Click(object sender, EventArgs e)
